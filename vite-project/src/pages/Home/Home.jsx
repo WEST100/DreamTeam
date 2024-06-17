fa290378661802664c91d1136853ea90617798a2
WEST100 <semeniv.denis@gmail.com>
2024-06-17 21:01:12 +0200

    denys: add styles for home-page

--- vite-project/src/pages/Home/Home.jsx
+++ vite-project/src/pages/Home/Home.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
+import React from "react";
+// import "./App.scss";
+import logo from "/src/assets/images/home_img/logo.png"
+import heart from "/src/assets/images/home_img/heart.svg";
+import cart from "/src/assets/images/home_img/cart.svg";
+import header from "/src/assets/images/home_img/header.jpg";
+import { NavLink } from "react-router-dom";
+
+
 
 const Home = () => {
   return (
-    <div>
-      It's a HomePage
-    </div>
-  )
-}
+    <>
+      <header className="header">
+        <div className="container">
+          <div className="company-style">
+            <div className="logo">
+              <img src={logo} alt="logo" />
+            </div>
+            <div className="theme">
+              <input type="text" />
+            </div>
+          </div>
+          <div className="menu">
+            <div className="menu__button">
+              <button className="menu__button_small montserrat-main">1 day discount!</button>
+            </div>
+            <nav className="nav">
+              <ul className="menu__list montserrat-main">
+                <li className="menu__item">
+                  <NavLink to="/">Main Page</NavLink>
+                </li>
+                <li className="menu__item">
+                  <NavLink to="/categories">Categories</NavLink>
+                </li>
+                <li className="menu__item">
+                  <NavLink to="/products">All products</NavLink>
+                </li>
+                <li className="menu__item">
+                  <a href="#">All sales</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="users-menu">
+            <a href="#">
+              <img src={heart} alt="favourite" />
+            </a>
+            <a href="#">
+              <img src={cart} alt="cart" />
+            </a>
+          </div>
+        </div>
+      </header>
+      <section className="main-content">
+        <div className="container">
+          <div className="main-content__items">
+            <div className="main-content__item">
+              <h1 className="main__text">
+                Amazing Discounts <br />
+                on Garden Products!
+              </h1>
+              <button className="main__button_large">Check out</button>
+            </div>
+          </div>
+        </div>
+        <div className="main-content__image">
+          <img className="main-image" src={header} alt="header_image" />
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
