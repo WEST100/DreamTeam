2178c6f5c2ceeb55a0644e6138c6f6bd5a4ad238
WEST100 <semeniv.denis@gmail.com>
2024-06-18 15:07:36 +0200

    denys: fix bug with position absolut in first section in homepage

--- vite-project/src/pages/Home/Home.jsx
+++ vite-project/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import "./App.scss";
+import "./Home.scss";
 import logo from "/src/assets/images/home_img/logo.png";
 import heart from "/src/assets/images/home_img/heart.svg";
 import cart from "/src/assets/images/home_img/cart.svg";
@@ -21,10 +21,10 @@ const Home = () => {
           </div>
           <div className="menu">
             <div className="menu__button">
-              <button className="menu__button_small montserrat-main">1 day discount!</button>
+              <button className="menu__button_small">1 day discount!</button>
             </div>
             <nav className="nav">
-              <ul className="menu__list montserrat-main">
+              <ul className="menu__list">
                 <li className="menu__item">
                   <NavLink to="/">Main Page</NavLink>
                 </li>
@@ -51,19 +51,17 @@ const Home = () => {
         </div>
       </header>
       <section className="main-content">
-        <div className="container">
-          <div className="main-content__items">
-            <div className="main-content__item">
+        <div className="main-content__items">
+            <img className="main-image" src={header} alt="header_image" />
+          <div className="main-content__item">
+            <div className="container">
               <h1 className="main__text">
                 Amazing Discounts <br />
                 on Garden Products!
               </h1>
-              <button className="main__button_large montserrat-main">Check out</button>
+              <button className="main__button_large">Check out</button>
             </div>
           </div>
-        </div>
-        <div className="main-content__image">
-          <img className="main-image" src={header} alt="header_image" />
         </div>
       </section>
     </>
