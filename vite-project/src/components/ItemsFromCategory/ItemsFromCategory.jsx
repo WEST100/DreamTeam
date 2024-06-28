469926a4247145ad52303eded2396742ad87ed55
WEST100 <semeniv.denis@gmail.com>
2024-06-28 18:54:10 +0200

    issue-21: work on ItemsFromCategore.jsx

--- vite-project/src/components/ItemsFromCategory/ItemsFromCategory.jsx
+++ vite-project/src/components/ItemsFromCategory/ItemsFromCategory.jsx
@@ -4,13 +4,28 @@ import ProductCard from "../Product/ProductCard";
 
 export default function ItemsFromCategory({ product }) {
   const { categoryId } = useParams();
-  console.log(product.image);
 
-  newProductsArray = [product];
+  let newProductsArray = [product];
+  // console.log(newProductsArray);
 
-  return {
-  
-  };
+  return (
+    <div className="productCardContainer">
+      {newProductsArray &&
+        newProductsArray.filter((item) => {
+          if (item.categoryId == categoryId) {
+            <ProductCard />;
+            // console.log('ok');
+            // <>
+            //   <img className="productCardImg" src={`/src/assets/images${item.image}`} alt="Product img" />
+            //   <h2>{item.title}</h2>
+            //   <p>{item.price}</p>
+            //   <p>{item.discont_price}</p>
+            //   <p>{item.description}</p>
+            // </>;
+          }
+        })}
+    </div>
+  );
   // <>
   // <ProductCard/>
   // </>
@@ -23,14 +38,12 @@ export default function ItemsFromCategory({ product }) {
   // </div>
 }
 
-      
-        // if (product.categoryId === categoryId) {
-        //     <>
-        //       <img className="productCardImg" src={`/src/assets/images${product.image}`} alt="Product img" />
-        //       <h2>{product.title}</h2>
-        //       <p>{product.price}</p>
-        //       <p>{product.discont_price}</p>
-        //       <p>{product.description}</p>
-        //     </>;
-        //   }
-      
+// if (product.categoryId === categoryId) {
+//     <>
+//       <img className="productCardImg" src={`/src/assets/images${product.image}`} alt="Product img" />
+//       <h2>{product.title}</h2>
+//       <p>{product.price}</p>
+//       <p>{product.discont_price}</p>
+//       <p>{product.description}</p>
+//     </>;
+//   }
