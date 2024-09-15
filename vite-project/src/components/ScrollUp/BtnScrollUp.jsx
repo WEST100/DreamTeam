27a445181b0982ea8b4bf66fc9683b35c4d77542
WEST100 <semeniv.denis@gmail.com>
2024-09-15 16:34:11 +0200

    adaptive bugs on cart are fixed

--- vite-project/src/components/ScrollUp/BtnScrollUp.jsx
+++ vite-project/src/components/ScrollUp/BtnScrollUp.jsx
@@ -1,20 +1,43 @@
-import React from 'react';
-import './BtnScrollUp.scss'
+import React, { useState, useEffect } from 'react';
+import './BtnScrollUp.scss';
 
 function BtnScrollUp() {
-	
-	const handlerScrollUp = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Функция для обработки прокрутки страницы
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  // Обработчик для прокрутки страницы вверх
+  const handlerScrollUp = () => {
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: 'smooth',
     });
-	}
-	
-	return (
-		<div className={'up'} onClick={handlerScrollUp}>&#9650;</div>
-	);
+  };
+
+  // useEffect для добавления и удаления слушателя прокрутки
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <>
+      {isVisible && (
+        <div className={'up'} onClick={handlerScrollUp}>&#9650;</div>
+      )}
+    </>
+  );
 }
 
 export default BtnScrollUp;
-
